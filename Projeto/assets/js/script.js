11cc70b9583d0b77ed5712db78bb5dd61150f978
Vasc0Marques <145674802+Vasc0Marques@users.noreply.github.com>
2023-11-20 13:37:27 +0000

    Add files via upload

--- Projeto/assets/js/script.js
+++ Projeto/assets/js/script.js
@@ -1,2 +1,19 @@
 // Código JavaScript aqui
 // Você pode começar adicionando interatividade à sua página
+
+        // Funções para incrementar e decrementar a quantidade
+    function adicionar(id) {
+        var quantityInput = document.getElementById('quantity'+id);
+        var quantityValue = parseInt(quantityInput.value, 10);
+        quantityInput.value = quantityValue + 1;
+    }
+
+    function retirar(id) {
+        var quantityInput = document.getElementById('quantity'+id);
+        var quantityValue = parseInt(quantityInput.value, 10);
+        
+        // Certifique-se de que a quantidade não seja menor que 1
+        if (quantityValue > 1) {
+            quantityInput.value = quantityValue - 1;
+        }
+    }
