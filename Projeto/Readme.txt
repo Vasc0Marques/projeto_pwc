582ff1703caf39ca08a06c8c51aba8000c51623a
Vasc0Marques <145674802+Vasc0Marques@users.noreply.github.com>
2024-01-18 18:07:44 +0000

    Add files via upload

---
+++ Projeto/Readme.txt
@@ -0,0 +1,6 @@
+PSI_PWC_PL3-G_Vasco.Marques_Antonio.Bucete_Ivo.Simões.zip
+https://github.com/Vasc0Marques/projeto_pwc.git
+2232347 Vasco Marques
+2232354 Antonio Bucete
+2231695 Ivo Simões
+Regime - Diurno
--- Projeto/assets/js/script.js
+++ Projeto/assets/js/script.js
@@ -1,7 +1,4 @@
-// Código JavaScript aqui
-// Você pode começar adicionando interatividade à sua página
 
-        // Funções para incrementar e decrementar a quantidade
     function adicionar(id) {
         var quantityInput = document.getElementById('quantity'+id);
         var quantityValue = parseInt(quantityInput.value, 10);
@@ -12,14 +9,13 @@
         var quantityInput = document.getElementById('quantity'+id);
         var quantityValue = parseInt(quantityInput.value, 10);
         
-        // Certifique-se de que a quantidade não seja menor que 1
         if (quantityValue > 1) {
             quantityInput.value = quantityValue - 1;
         }
     }
 
     function initMap() {
-        var iplLatLng = {lat: 39.735129, lng: -8.820732}; // Coordenadas aproximadas do Instituto Politécnico de Leiria
+        var iplLatLng = {lat: 39.735129, lng: -8.820732}; 
     
         var map = new google.maps.Map(document.getElementById('map'), {
             center: iplLatLng,
@@ -32,3 +28,63 @@
             title: 'Instituto Politécnico de Leiria (IPL)'
         });
     }
+
+    function adjustSelector() {
+        var tabsNewAnim = $('.navbar');
+        var selectorNewAnim = $('.navbar').find('li').length;
+        var activeItemNewAnim = tabsNewAnim.find('.active');
+        var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+        var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+        var itemPosNewAnimTop = activeItemNewAnim.position();
+        var itemPosNewAnimLeft = activeItemNewAnim.position();
+        $(".hori-selector").css({
+            "top": itemPosNewAnimTop.top + "px",
+            "left": itemPosNewAnimLeft.left + "px",
+            "height": activeWidthNewAnimHeight + "px",
+            "width": activeWidthNewAnimWidth + "px"
+        });
+        $(".navbar").on("click", "li", function (e) {
+            $('.navbar ul li').removeClass("active");
+            $(this).addClass('active');
+            var activeWidthNewAnimHeight = $(this).innerHeight();
+            var activeWidthNewAnimWidth = $(this).innerWidth();
+            var itemPosNewAnimTop = $(this).position();
+            var itemPosNewAnimLeft = $(this).position();
+            $(".hori-selector").css({
+                "top": itemPosNewAnimTop.top + "px",
+                "left": itemPosNewAnimLeft.left + "px",
+                "height": activeWidthNewAnimHeight + "px",
+                "width": activeWidthNewAnimWidth + "px"
+            });
+        });
+    }
+    
+    $(document).ready(function () {
+        setTimeout(function () {
+            adjustSelector();
+        });
+    });
+    
+    $(window).on('resize', function () {
+        setTimeout(function () {
+            adjustSelector();
+        }, 500);
+    });
+    
+    $(".navbar-toggler").click(function () {
+        $(".navbar-collapse").slideToggle(300);
+        setTimeout(function () {
+            adjustSelector();
+        });
+    });
+    
+    // Add active class to the current page link
+    jQuery(document).ready(function ($) {
+        var path = window.location.pathname.split("/").pop();
+        if (path === '') {
+            path = 'index.html';
+        }
+        var target = $('.navbar ul li a[href="' + path + '"]');
+        target.parent().addClass('active');
+    });
+    
